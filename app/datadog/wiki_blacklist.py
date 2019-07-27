14e5608407e39fafa06282d3a9f0d16ba01e5a11
Matthew Flood <hermannwest@gmail.com>
2019-07-27 16:10:07 -0600

    create wiki_blacklist.py

---
+++ app/datadog/wiki_blacklist.py
@@ -0,0 +1,86 @@
+
+
+import logging
+import os
+import sys
+import requests
+from datadog.loggingsetup import LOGNAME
+
+BLACKLIST_URL="https://s3.amazonaws.com/dd-interview-data/data_engineer/wikipedia/blacklist_domains_and_pages"
+LOCAL_BLACKLIST_FILEPATH="/tmp/datadog_wiki_blacklist"
+
+
+class WikiBlacklist():
+    """
+        Encapsulates blacklist
+    """
+
+    def __init__(self):
+        self._logger = logging.getLogger(LOGNAME)
+        self._blacklist = {}
+        self._local_filepath = LOCAL_BLACKLIST_FILEPATH
+
+    def get_size(self):
+        """
+            Return the number of items in the list
+        """
+        return len(self._blacklist)
+        
+    def head(self):
+        """
+            Show 20 entriew from the blacklist
+        """
+        keys = list(self._blacklist.keys())[:20]
+        for k in keys:
+            self._logger.info("blacklist %s = %s", k, self._blacklist[k])
+
+    def _download_blacklist_file(self, url, local_filepath):
+        """
+           Download a remote file to local_filepath 
+           Do it in chunks so we don't have the entire file in memory
+        """
+        self._logger.info("Downloading %s to %s", url, local_filepath)
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_filepath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk: # filter out keep-alive new chunks
+                        sys.stderr.write(".")
+                        sys.stderr.flush()
+                        #self._logger.debug(len(chunk))
+                        f.write(chunk)
+
+
+    def load_list(self, force_download=False):
+        """
+            possible download blacklist file
+            and then parse it to load self._blacklist
+        """
+        if not os.path.exists(self._local_filepath) or force_download:
+            self._download_blacklist_file(url=BLACKLIST_URL,
+                                          local_filepath=self._local_filepath)
+        self._blacklist = self._parse_file(local_filepath=self._local_filepath)
+        self._logger.info("Loaded %d blacklist entries", len(self._blacklist))
+
+    def _parse_file(self, local_filepath):
+        """
+            parses file, returns dictionary
+            of the contents
+        """
+        self._logger.info("Loading blacklist data from %s", local_filepath)
+        return_dict = {}
+        with open(local_filepath, "r") as handle:
+            for line in handle:
+                line = line.strip()#.lower()
+                return_dict[line] = 1
+
+        return return_dict 
+
+    def is_blacklisted(self, page):
+        """
+            return true of page is in the blacklist
+        """
+        if page in self._blacklist:
+            return True
+        return False
+
---
+++ app/tests/test_wikipedia_blacklist.py
@@ -0,0 +1,46 @@
+
+import logging
+import pytest
+import requests_mock
+from datadog import loggingsetup
+from datadog.wiki_blacklist import WikiBlacklist
+from datadog.wiki_blacklist import BLACKLIST_URL
+loggingsetup.init(logging.DEBUG)
+
+@pytest.fixture()
+def req_mock():
+    """
+        # info on requests mocking
+        # usually requests_mock would be available as fixture
+        https://requests-mock.readthedocs.io/en/latest/pytest.html
+        # but requests_mock not available as fixture in python 3
+        https://github.com/pytest-dev/pytest/issues/2749
+        # hence, this fixture
+    """
+    with requests_mock.Mocker() as the_mocker:
+        yield the_mocker
+
+BLACK_LIST = """ab %D0%91%D0%BE%D1%80%D0%B8%D1%81_%D0%93%D1%80%D1%8B%D0%B7%D0%BB%D0%BE%D0%B2
+ace Beureukaih:Nuvola_apps_important.svg
+ace Japan
+ace Kusuih:Hubong_gisa/Ureu%C3%ABng_Ngui:Hoo_User_Page_Bot
+ace Kusuih:Neuubah_meuhubong/Seunaleu%C3%ABk:Flag
+ace Marit_Ureu%C3%ABng_Ngui:GennadyL
+ace.m Kusuih:MobileLanguages/Republik_Chuvash
+af .sy
+af 2009
+af Apostelskap"""
+
+def test_get_wiki_blacklist(req_mock):
+
+    # this sets up the mock_urtl with a mock response
+    req_mock.get(BLACKLIST_URL, text=BLACK_LIST)
+
+    wblk = WikiBlacklist()
+    wblk._local_filepath = "/tmp/teapot"
+    wblk.load_list(force_download=True)
+    wblk.head()
+    assert wblk.is_blacklisted(page="af Apostelskap") == True
+    assert wblk.is_blacklisted(page="af apostelskap") == False
+    assert wblk.get_size() == 10
+
