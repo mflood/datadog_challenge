8b40f860cb0466c3c994d25782aad7325e410b5f
Matthew Flood <hermannwest@gmail.com>
2019-07-27 20:56:58 -0600

    Create wiki_processor.py

---
+++ app/datadog/wiki_processor.py
@@ -0,0 +1,131 @@
+"""
+    wiki_processor.py
+
+    implements WikiProcessor
+"""
+
+import logging
+import gzip
+import os
+import heapq
+import sys
+import requests
+from datadog.loggingsetup import LOGNAME
+
+BASE_URL = "https://dumps.wikimedia.org/other/pageviews/"
+CACHE_DIR = "/tmp"
+TOP_N = 25
+IGNORE_DASH = True
+
+class WikiProcessor():
+    """
+        Knows how to download and process a wiki log file
+    """
+
+    def __init__(self, blacklist):
+        self._logger = logging.getLogger(LOGNAME)
+
+        # self._blacklist needs to implement is_blacklisted(page)
+        self._blacklist = blacklist
+
+    def get_url(self, year, month, day, hour):
+        """
+            return deterministic url filepath
+
+            e.g https://dumps.wikimedia.org/other/pageviews/2015/2015-05/pageviews-20150501-010000.gz
+        """
+        path = "{base_url}/{y:04d}/{y:04d}-{m:02d}/pageviews-{y:04d}{m:02d}{d:02d}-{h:02d}0000.gz".format(
+            base_url=BASE_URL.rstrip('/'),
+            y=int(year),
+            m=int(month),
+            d=int(day),
+            h=int(hour))
+        return path
+
+    def get_local_cache_path(self, year, month, day, hour):
+        """
+            return deterministic local filepath to cache the wiki data
+
+            e.g. /tmp/2017.12.03.00.cache
+        """
+        path = "{}/{:04d}.{:02d}.{:02d}.{:02d}.cache".format(CACHE_DIR.rstrip('/'),
+                                                             int(year),
+                                                             int(month),
+                                                             int(day),
+                                                             int(hour))
+        return path
+
+    def _download_wiki_file(self, url, local_filepath):
+        """
+           Download a remote file to local_filepath
+           Do it in chunks so we don't have the entire file in memory
+        """
+        self._logger.info("Downloading %s to %s", url, local_filepath)
+        with requests.get(url, stream=True) as request:
+            request.raise_for_status()
+            with open(local_filepath, 'wb') as handle:
+                for chunk in request.iter_content(chunk_size=8192):
+                    if chunk: # filter out keep-alive new chunks
+                        sys.stderr.write(".")
+                        sys.stderr.flush()
+                        handle.write(chunk)
+
+    def process_pageviews(self, year, month, day, hour, force_download=False):
+        """
+            possibly download wiki file
+            and then process it
+            returning stats
+        """
+        local_path = self.get_local_cache_path(year, month, day, hour)
+
+        if not os.path.exists(local_path) or force_download:
+            url = self.get_url(year, month, day, hour)
+            self._download_wiki_file(url=url,
+                                     local_filepath=local_path)
+        stats = self._process_file(local_filepath=local_path)
+        return stats
+
+
+    def _process_filehandle(self, filehandle):
+
+
+        stats = {}
+        for index, raw_line in enumerate(filehandle):
+            line = raw_line.strip().decode()
+            #print("line is %s" % line)
+            #print("type is %s" % type(line))
+            parts = line.split(' ')
+            
+
+            stats.setdefault(parts[0], [])
+             
+            if IGNORE_DASH and parts[1] == '-':
+                continue
+
+            if not self._blacklist.is_blacklisted(page="{} {}".format(parts[0], parts[1])):
+                heapq.heappush(stats[parts[0]], 
+                                     (int(parts[2]), parts[1]))
+                if len(stats[parts[0]]) > TOP_N:
+                    # remove the lowest value
+                    heapq.heappop(stats[parts[0]])
+
+        return stats
+
+    def _process_file(self, local_filepath):
+        """
+            processes the localfile to gain stats
+            parses file, returns dictionary
+            of the stats
+        """
+        self._logger.info("Loading page data from %s", local_filepath)
+
+        # first try to process as plaintext file
+        ##try:
+        #   with open(local_filepath, "r") as handle:
+        #        return self._process_filehandle(handle)
+
+        #except UnicodeDecodeError: 
+        with gzip.open(local_filepath, 'rb') as handle:
+            return self._process_filehandle(handle)
+
+# end
---
+++ app/tests/test_wiki_processor.py
@@ -0,0 +1,130 @@
+"""
+    test_wiki_processor.py
+
+    unit tests for WikiProcessor
+"""
+
+import logging
+import pytest
+import requests_mock # pylint: disable=import-error
+from datadog import loggingsetup
+from datadog.wiki_blacklist import WikiBlacklist
+from datadog.wiki_processor import WikiProcessor
+loggingsetup.init(logging.DEBUG)
+
+SAMPLE_FILE = "tests/sample_pageviews.txt.Z"
+
+def page_content(arg1, arg2):
+    with open(SAMPLE_FILE, "rb") as handle:
+        content = handle.read()
+    return content
+
+
+@pytest.fixture()
+def blacklist_mock():
+    
+    class BlacklistMock():
+
+        def is_blacklisted(self, page):
+            if page == "commons.m Category:1962_Flood_in_Skopje":
+                return True
+
+    return BlacklistMock()
+
+@pytest.fixture()
+def req_mock():
+    """
+        # info on requests mocking
+        # usually requests_mock would be available as fixture
+        https://requests-mock.readthedocs.io/en/latest/pytest.html
+        # but requests_mock not available as fixture in python 3
+        https://github.com/pytest-dev/pytest/issues/2749
+        # hence, this fixture
+    """
+    with requests_mock.Mocker() as the_mocker:
+        yield the_mocker
+
+
+def test_get_url():
+    """
+        Test the get_url() method
+    """
+    processor = WikiProcessor(blacklist=None)
+    url = processor.get_url(year=2017, month=12, day=3, hour=0)
+    expected = "https://dumps.wikimedia.org/other/pageviews/2017/2017-12/pageviews-20171203-000000.gz"
+    assert url == expected
+
+def test_get_local_cache_path():
+    """
+        Test the get_local_cache_path() method
+    """
+    processor = WikiProcessor(blacklist=None)
+    path = processor.get_local_cache_path(year=2017, month=12, day=3, hour=0)
+    expected = "/tmp/2017.12.03.00.cache"
+    assert path == expected
+
+def test_process_pageviews(req_mock, blacklist_mock): # pylint: disable=redefined-outer-name
+    """
+        Test basic usage
+        use mock to download fake contents
+    """
+
+    year = 2010
+    month = 12
+    day = 3
+    hour = 0
+
+    processor = WikiProcessor(blacklist=blacklist_mock)
+    url = processor.get_url(year=year,
+                            month=month,
+                            day=day,
+                            hour=hour)
+
+    # this sets up the mock_url with a mock response
+    req_mock.get(url, content=page_content)
+
+    results = processor.process_pageviews(year=year,
+                                          month=month,
+                                          day=day,
+                                          hour=hour,
+                                          force_download=True)
+
+    
+    domains = list(results.keys())
+    domains.sort()
+    assert domains == ['as.m',
+                       'bn.m',
+                       'commons.m',
+                       'commons.m.m',
+                       'de',
+                       'de.m',
+                       'en',
+                       'en.d',
+                       'en.m',
+                       'en.m.d',
+                       'en.n',
+                       'es',
+                       'es.m',
+                       'fr',
+                       'fr.m',
+                       'id.m',
+                       'it',
+                       'it.m',
+                       'ja',
+                       'ja.m',
+                       'm.wd',
+                       'no',
+                       'pl',
+                       'pt.m',
+                       'ru.m',
+                       'simple',
+                       'simple.d',
+                       'simple.m',
+                       'sv.m',
+                       'uk']
+
+
+
+# end
+
+
