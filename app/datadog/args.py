5488021b614a90c3ac55052a710cfe65aacdb14d
Matthew Flood <hermannwest@gmail.com>
2019-07-28 09:56:38 -0600

    add ranges to args

--- app/datadog/args.py
+++ app/datadog/args.py
@@ -4,8 +4,12 @@
     defines args for the program
 """
 
+import sys
 import datetime
 import argparse
+
+class ParserException(Exception):
+    pass
 
 def make_date_from_string(date_string):
     """
@@ -40,6 +44,28 @@ def hour_argument(some_arg):
         raise TypeError("")
     return as_int
 
+
+def validate_args(args):
+    """
+        Does additional validation that is
+        too complex for argparse
+    """
+
+    # exclusive or
+    if bool(args.start_hour) != bool(args.end_hour):
+        raise ParserException("Hour range requires --start-hour and --end-hour")
+
+    if args.start_hour and args.end_hour < args.start_hour:
+        raise ParserException("--end-hour cannot be less than --start-hour")
+
+    # exclusive or
+    if bool(args.start_date) != bool(args.end_date):
+        raise ParserException("Date range requires --start-date and --end-date")
+
+    if args.start_date and args.end_date < args.start_date:
+        raise ParserException("--end-date cannot be less than --start-date")
+
+
 def parse_args(argv=None):
     """
         Parse command line args
@@ -68,7 +94,47 @@ def parse_args(argv=None):
                         type=hour_argument,
                         help="Hour of data to analyze (default current hour)")
 
+    # range related options
+    #
+    range_group = parser.add_argument_group(title='Range Options')
+
+    range_group.add_argument("--start-date",
+                        dest="start_date",
+                        metavar='YYYY-MM-DD',
+                        required=False,
+                        type=date_argument,
+                        help="Start date of date range (inclusive)")
+
+    range_group.add_argument("--end-date",
+                        dest="end_date",
+                        metavar='YYYY-MM-DD',
+                        required=False,
+                        type=date_argument,
+                        help="End date of date range (inclusive)")
+
+    range_group.add_argument("--start-hour",
+                        dest="start_hour",
+                        metavar='H',
+                        type=hour_argument,
+                        required=False,
+                        help="Start of hour range (0-23)")
+
+    range_group.add_argument("--end-hour",
+                        dest="end_hour",
+                        metavar='H',
+                        type=hour_argument,
+                        required=False,
+                        help="End of hour range (0-23)")
+
     results = parser.parse_args(argv)
+
+    try:
+        validate_args(args=results)
+    except ParserException as e:
+        sys.stderr.write("{}\n".format(e))
+        parser.print_help()
+        sys.exit(2)
+
     return results
 
 # end
--- app/tests/test_args.py
+++ app/tests/test_args.py
@@ -60,3 +60,36 @@ def test_invalid_hour():
 
     with pytest.raises(SystemExit):
         parse_args(["--hour", "-1"])
+
+def test_date_ranges():
+    """
+        date range must be legit
+    """
+    # incomplete range
+    with pytest.raises(SystemExit):
+        parse_args(["--start-date", "2019-04-01"])
+
+    # incomplete range
+    with pytest.raises(SystemExit):
+        parse_args(["--end-date", "2019-04-01"])
+
+    # start after end
+    with pytest.raises(SystemExit) as some_error:
+        parse_args(["--start-date", "2019-04-28",
+                    "--end-date", "2019-04-01"])
+
+def test_hour_ranges():
+    """
+        hour range must be legit
+    """
+    # incomplete range
+    with pytest.raises(SystemExit):
+        parse_args(["--start-hour", "2"])
+
+    # incomplete range
+    with pytest.raises(SystemExit):
+        parse_args(["--end-hour", "2"])
+
+    # start after end
+    with pytest.raises(SystemExit):
+        parse_args(["--start-hour", "4", "--end-hour", "2"])
